f04c977c5fb1bf73844dab39a73aca1b8e329501
Taylor Berry <taylorberry019@gmail.com>
2022-01-21 10:44:58 -0700

    env entered

---
+++ src/env.js
@@ -0,0 +1,8 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://bcw-sandbox.herokuapp.com/' : ''
+export const useSockets = false
+export const domain = 'codeworksclassroom.auth0.com'
+export const clientId = 'OXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const audience = 'https://codeworksclassroom.com'
+
+
